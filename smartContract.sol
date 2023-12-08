a9f53f678350bf3f11f0b879e8d0487c2e776f05
gpl2210 <73338163+gpl2210@users.noreply.github.com>
2023-12-08 23:15:41 +0530

    Update smartContract.sol

--- smartContract.sol
+++ smartContract.sol
@@ -1,1 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ControlOnAnger is ERC20, Ownable {
+    mapping(address => bool) private ItemsRedeemed;
+    uint public constant Loot = 10* 10;
+
+    constructor() ERC20("ControlOnAnger", "COA") {
+        _mint(msg.sender, Loot);
+    }
+
+    function mint(address acc, uint amo) public onlyOwner {
+        _mint(acc, amo);
+    }
+
+    function redeemItem() public {
+        require(balanceOf(msg.sender) >= Loot, "Balance is insufficient");
+        require(!ItemsRedeemed[msg.sender], "Items are redeemed already");
+
+        ItemsRedeemed[msg.sender] = true;
+        _burn(msg.sender, Loot);
+    }
+
+    function burn(uint amo) public {
+        _burn(msg.sender, amo);
+    }
+}
